fdd7125b355df7a47623cb5730e75a91cb4eb2bf
thangle98 <leanhthang10a1@gmail.com>
2020-12-06 08:20:40 +0700

    Load_data_Frm_NXB

--- QuanLyThuVienHVKTQS/frmNhaXuatBan.Designer.cs
+++ QuanLyThuVienHVKTQS/frmNhaXuatBan.Designer.cs
@@ -64,9 +64,10 @@
             this.columnHeader5});
             this.listView_NXB.FullRowSelect = true;
             this.listView_NXB.GridLines = true;
-            this.listView_NXB.Location = new System.Drawing.Point(2, 1);
+            this.listView_NXB.Location = new System.Drawing.Point(3, 1);
+            this.listView_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listView_NXB.Name = "listView_NXB";
-            this.listView_NXB.Size = new System.Drawing.Size(379, 363);
+            this.listView_NXB.Size = new System.Drawing.Size(504, 446);
             this.listView_NXB.TabIndex = 0;
             this.listView_NXB.UseCompatibleStateImageBehavior = false;
             this.listView_NXB.View = System.Windows.Forms.View.Details;
@@ -99,36 +100,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Location = new System.Drawing.Point(23, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(59, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Mã NXB";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 67);
+            this.label2.Location = new System.Drawing.Point(23, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.Size = new System.Drawing.Size(65, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tên NXB";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 105);
+            this.label3.Location = new System.Drawing.Point(23, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.Size = new System.Drawing.Size(51, 17);
             this.label3.TabIndex = 3;
             this.label3.Text = "Địa chỉ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 144);
+            this.label4.Location = new System.Drawing.Point(8, 177);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.Size = new System.Drawing.Size(91, 17);
             this.label4.TabIndex = 4;
             this.label4.Text = "Số điện thoại";
             // 
@@ -142,38 +147,44 @@
             this.group_NXB.Controls.Add(this.label4);
             this.group_NXB.Controls.Add(this.label2);
             this.group_NXB.Controls.Add(this.label3);
-            this.group_NXB.Location = new System.Drawing.Point(381, 1);
+            this.group_NXB.Location = new System.Drawing.Point(508, 1);
+            this.group_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.group_NXB.Name = "group_NXB";
-            this.group_NXB.Size = new System.Drawing.Size(309, 243);
+            this.group_NXB.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.group_NXB.Size = new System.Drawing.Size(412, 299);
             this.group_NXB.TabIndex = 5;
             this.group_NXB.TabStop = false;
             // 
             // sdtnxbtxt
             // 
-            this.sdtnxbtxt.Location = new System.Drawing.Point(92, 141);
+            this.sdtnxbtxt.Location = new System.Drawing.Point(123, 174);
+            this.sdtnxbtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sdtnxbtxt.Name = "sdtnxbtxt";
-            this.sdtnxbtxt.Size = new System.Drawing.Size(100, 20);
+            this.sdtnxbtxt.Size = new System.Drawing.Size(132, 22);
             this.sdtnxbtxt.TabIndex = 8;
             // 
             // diachinxbtxt
             // 
-            this.diachinxbtxt.Location = new System.Drawing.Point(92, 102);
+            this.diachinxbtxt.Location = new System.Drawing.Point(123, 126);
+            this.diachinxbtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.diachinxbtxt.Name = "diachinxbtxt";
-            this.diachinxbtxt.Size = new System.Drawing.Size(100, 20);
+            this.diachinxbtxt.Size = new System.Drawing.Size(132, 22);
             this.diachinxbtxt.TabIndex = 7;
             // 
             // tennxbtxt
             // 
-            this.tennxbtxt.Location = new System.Drawing.Point(92, 64);
+            this.tennxbtxt.Location = new System.Drawing.Point(123, 79);
+            this.tennxbtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tennxbtxt.Name = "tennxbtxt";
-            this.tennxbtxt.Size = new System.Drawing.Size(100, 20);
+            this.tennxbtxt.Size = new System.Drawing.Size(132, 22);
             this.tennxbtxt.TabIndex = 6;
             // 
             // manxbtxt
             // 
-            this.manxbtxt.Location = new System.Drawing.Point(92, 24);
+            this.manxbtxt.Location = new System.Drawing.Point(123, 30);
+            this.manxbtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.manxbtxt.Name = "manxbtxt";
-            this.manxbtxt.Size = new System.Drawing.Size(100, 20);
+            this.manxbtxt.Size = new System.Drawing.Size(132, 22);
             this.manxbtxt.TabIndex = 5;
             // 
             // groupnxb_btn
@@ -184,74 +195,86 @@
             this.groupnxb_btn.Controls.Add(this.Xoa_NXB);
             this.groupnxb_btn.Controls.Add(this.Sua_NXB);
             this.groupnxb_btn.Controls.Add(this.Them_NXB);
-            this.groupnxb_btn.Location = new System.Drawing.Point(381, 250);
+            this.groupnxb_btn.Location = new System.Drawing.Point(508, 308);
+            this.groupnxb_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupnxb_btn.Name = "groupnxb_btn";
-            this.groupnxb_btn.Size = new System.Drawing.Size(309, 114);
+            this.groupnxb_btn.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupnxb_btn.Size = new System.Drawing.Size(412, 140);
             this.groupnxb_btn.TabIndex = 6;
             this.groupnxb_btn.TabStop = false;
             // 
             // Thoat_NXB
             // 
-            this.Thoat_NXB.Location = new System.Drawing.Point(212, 72);
+            this.Thoat_NXB.Location = new System.Drawing.Point(283, 89);
+            this.Thoat_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Thoat_NXB.Name = "Thoat_NXB";
-            this.Thoat_NXB.Size = new System.Drawing.Size(75, 23);
+            this.Thoat_NXB.Size = new System.Drawing.Size(100, 28);
             this.Thoat_NXB.TabIndex = 5;
             this.Thoat_NXB.Text = "Thoát";
             this.Thoat_NXB.UseVisualStyleBackColor = true;
             // 
             // Boqua_NXB
             // 
-            this.Boqua_NXB.Location = new System.Drawing.Point(106, 72);
+            this.Boqua_NXB.Location = new System.Drawing.Point(141, 89);
+            this.Boqua_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Boqua_NXB.Name = "Boqua_NXB";
-            this.Boqua_NXB.Size = new System.Drawing.Size(75, 23);
+            this.Boqua_NXB.Size = new System.Drawing.Size(100, 28);
             this.Boqua_NXB.TabIndex = 4;
             this.Boqua_NXB.Text = "Bỏ qua";
             this.Boqua_NXB.UseVisualStyleBackColor = true;
             // 
             // Luu_NXB
             // 
-            this.Luu_NXB.Location = new System.Drawing.Point(6, 72);
+            this.Luu_NXB.Location = new System.Drawing.Point(8, 89);
+            this.Luu_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Luu_NXB.Name = "Luu_NXB";
-            this.Luu_NXB.Size = new System.Drawing.Size(75, 23);
+            this.Luu_NXB.Size = new System.Drawing.Size(100, 28);
             this.Luu_NXB.TabIndex = 3;
             this.Luu_NXB.Text = "Lưu";
             this.Luu_NXB.UseVisualStyleBackColor = true;
             // 
             // Xoa_NXB
             // 
-            this.Xoa_NXB.Location = new System.Drawing.Point(212, 19);
+            this.Xoa_NXB.Location = new System.Drawing.Point(283, 23);
+            this.Xoa_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Xoa_NXB.Name = "Xoa_NXB";
-            this.Xoa_NXB.Size = new System.Drawing.Size(75, 23);
+            this.Xoa_NXB.Size = new System.Drawing.Size(100, 28);
             this.Xoa_NXB.TabIndex = 2;
             this.Xoa_NXB.Text = "Xóa";
             this.Xoa_NXB.UseVisualStyleBackColor = true;
+            this.Xoa_NXB.Click += new System.EventHandler(this.Xoa_NXB_Click);
             // 
             // Sua_NXB
             // 
-            this.Sua_NXB.Location = new System.Drawing.Point(106, 19);
+            this.Sua_NXB.Location = new System.Drawing.Point(141, 23);
+            this.Sua_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Sua_NXB.Name = "Sua_NXB";
-            this.Sua_NXB.Size = new System.Drawing.Size(75, 23);
+            this.Sua_NXB.Size = new System.Drawing.Size(100, 28);
             this.Sua_NXB.TabIndex = 1;
             this.Sua_NXB.Text = "Sửa";
             this.Sua_NXB.UseVisualStyleBackColor = true;
+            this.Sua_NXB.Click += new System.EventHandler(this.Sua_NXB_Click);
             // 
             // Them_NXB
             // 
-            this.Them_NXB.Location = new System.Drawing.Point(6, 19);
+            this.Them_NXB.Location = new System.Drawing.Point(8, 23);
+            this.Them_NXB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Them_NXB.Name = "Them_NXB";
-            this.Them_NXB.Size = new System.Drawing.Size(75, 23);
+            this.Them_NXB.Size = new System.Drawing.Size(100, 28);
             this.Them_NXB.TabIndex = 0;
             this.Them_NXB.Text = "Thêm";
             this.Them_NXB.UseVisualStyleBackColor = true;
+            this.Them_NXB.Click += new System.EventHandler(this.Them_NXB_Click);
             // 
             // frmNhaXuatBan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(691, 376);
+            this.ClientSize = new System.Drawing.Size(921, 463);
             this.Controls.Add(this.groupnxb_btn);
             this.Controls.Add(this.group_NXB);
             this.Controls.Add(this.listView_NXB);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmNhaXuatBan";
             this.Text = "frmNhaXuatBan";
             this.group_NXB.ResumeLayout(false);
--- QuanLyThuVienHVKTQS/frmNhaXuatBan.cs
+++ QuanLyThuVienHVKTQS/frmNhaXuatBan.cs
@@ -14,10 +14,65 @@ namespace QuanLyThuVienHVKTQS
 {
     public partial class frmNhaXuatBan : Form
     {
+        List<nhaxuatban> l = null;
+        private bool Them_bool = false;
+        private bool Sua_bool = true;
         public frmNhaXuatBan()
         {
             InitializeComponent();
+            HienThi_NXB();
         }
-        
+
+        private void btn_enable(bool t)
+        {
+            group_NXB.Enabled = t;
+            Them_NXB.Enabled = !t;
+            Sua_NXB.Enabled = !t;
+            Xoa_NXB.Enabled = !t;
+            Luu_NXB.Enabled = t;
+            Boqua_NXB.Enabled = t;
+            Them_bool = Sua_bool = false;
+        }
+        public void HienThi_NXB()
+        {
+            var nxb = new NhaXuatBanController();
+            l = nxb.Detail();
+            listView_NXB.Items.Clear();
+            int i = 1;
+
+            foreach (nhaxuatban row in l)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = "" + i++;
+                item.SubItems.Add(row.manxb.ToString());
+                item.SubItems.Add(row.tennxb.ToString());
+                item.SubItems.Add(row.diachi.ToString());
+                item.SubItems.Add(row.sdt.ToString());
+                listView_NXB.Items.Add(item);
+            }
+            if (PhanQuyen.quyen == 1)
+                btn_enable(false);
+            else
+            {
+                group_NXB.Enabled = false;
+                Them_NXB.Enabled = Xoa_NXB.Enabled = Sua_NXB.Enabled = Luu_NXB.Enabled = Boqua_NXB.Enabled = false;
+                Thoat_NXB.Enabled = true;
+            }
+        }
+
+        private void Them_NXB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sua_NXB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Xoa_NXB_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
